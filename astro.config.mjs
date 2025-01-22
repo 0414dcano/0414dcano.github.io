0488cc07964f660677f3275ba3b08eaf51aa96f6
0414dcano <100593419+0414dcano@users.noreply.github.com>
2025-01-22 18:59:11 +0100

    cambio config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://0414dcano.github.io/",
+  site: "https://0414dcano.github.io",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
